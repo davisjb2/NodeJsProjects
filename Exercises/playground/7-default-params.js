67c6eda84689ec6f0304388590fc85bbd92f57c8
Brooke Tibbett <davisjb2@appstate.edu>
2019-08-28 12:46:48 -0400

    trying deploy

---
+++ Exercises/playground/7-default-params.js
@@ -0,0 +1,7 @@
+const greeter = (name = "Ashley", age = 24) => {
+    console.log('Hello ' + name)
+}
+
+greeter('Brooke', 23)
+
+greeter()
---
+++ Projects/web-server-weather/public/js/app.js
@@ -0,0 +1,24 @@
+console.log("Client side javascript file is loaded");
+
+const weatherFrom = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = 'Loading...'
+
+weatherFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value
+    
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.summary + " It is currently " + data.temperature + " degrees outside. There is a " + data.precip + "% chance of rain."
+            }
+        })
+    })
+});
