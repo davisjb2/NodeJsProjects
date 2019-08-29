27bdbabc9e7026534a03e9579ad17f7b23dcae25
Brooke Tibbett <davisjb2@appstate.edu>
2019-08-29 16:55:22 -0400

    leaving for day

--- Projects/task-manager/src/db/mongoose.js
+++ Projects/task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,10 +8,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
@@ -27,16 +31,46 @@ task.save().then(() => {
 
 // const User = mongoose.model('User', {
 //     name: {
-//         type: String
+//         type: String,
+//         required: true,
+//         trim: true
 //     },
 //     age: {
-//         type: Number
+//         type: Number,
+//         validate(value) {
+//             if(value < 0) {
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         validate(value) {
+//             if(!validator.isEmail(value)) {
+//                 throw new Error('Not valid email')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value) {
+//             if(value.includes('password')) {
+//                 throw new Error('Password cannot contain "password"')
+//             }
+//         }
 //     }
 // })
 
 // const me = new User({
 //     name: 'Jessica',
-//     age: 23
+//     age: 23,
+//     email: 'b.cookie199@yahoo.com',
+//     password: 'passwooooooooord'
 // });
 
 // me.save().then(() => {
