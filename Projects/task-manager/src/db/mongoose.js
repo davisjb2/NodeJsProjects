65dc6ca3d7ed2d3af9c104156e3b29bbd1d431c3
Brooke Tibbett <davisjb2@appstate.edu>
2019-08-30 16:49:23 -0400

    leaving for day

--- Projects/task-manager/src/db/mongoose.js
+++ Projects/task-manager/src/db/mongoose.js
@@ -1,80 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
-const task = new Task({
-    description: 'Make Dinner',
-    completed: false
-});
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error);
-})
-
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     age: {
-//         type: Number,
-//         validate(value) {
-//             if(value < 0) {
-//                 throw new Error('Age must be a positive number')
-//             }
-//         }
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         validate(value) {
-//             if(!validator.isEmail(value)) {
-//                 throw new Error('Not valid email')
-//             }
-//         }
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         minlength: 7,
-//         validate(value) {
-//             if(value.includes('password')) {
-//                 throw new Error('Password cannot contain "password"')
-//             }
-//         }
-//     }
-// })
-
-// const me = new User({
-//     name: 'Jessica',
-//     age: 23,
-//     email: 'b.cookie199@yahoo.com',
-//     password: 'passwooooooooord'
-// });
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log(error)
-// });
